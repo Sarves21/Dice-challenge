203bd14c5b375855b93c1457e79719fee3f2472a
Sarves21 <123927237+Sarves21@users.noreply.github.com>
2024-06-02 01:53:58 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,16 @@
+var randomNumber1 = Math.floor(Math.random() * 6) + 1;
+var randomNumber2 = Math.floor(Math.random() * 6) + 1;
+
+var player1 = "./images/dice" + randomNumber1 + ".png";
+var player2 = "./images/dice" + randomNumber2 + ".png";
+
+document.querySelector(".dice .img1").setAttribute("src", player1);
+document.querySelector(".dice .img2").setAttribute("src", player2);
+
+if (randomNumber1 > randomNumber2) {
+  document.querySelector(".container h1").innerHTML = "🏆🥇Player 1 Wins😁";
+} else if (randomNumber2 > randomNumber1) {
+  document.querySelector(".container h1").innerHTML = "😁Player 2 Wins🏆🥇";
+} else if (randomNumber1 === randomNumber2) {
+  document.querySelector(".container h1").innerHTML = "Draw!";
+}
